0c33c4afda1d3e1e1d57f076c3e4420dad6d7c7d
Irene Scott <irenejinscott@gmail.com>
2020-06-03 20:10:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,11 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books JOIN books ON series.id = books.series_id ORDER BY books.year = 1;"
+  "SELECT books.title, books.year FROM books JOIN series ON books.series_id = series.id  ORDER BY books.year = 1;"
 end
-
+SELECT column(s)
+FROM table_one
+INNER JOIN table_two
+ON table_one.column_name = table_two.column_name
+WHERE table_two.column_name = condition;
 
 
 def select_name_and_motto_of_char_with_longest_motto
