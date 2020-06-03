a8216d1c70a84e2688c414bba0a77ea8ac3f48bb
Irene Scott <irenejinscott@gmail.com>
2020-06-03 21:00:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, FROM series JOIN ORDER BY LENGTH(characters) DESC LIMIT 1"
+  "SELECT series.title, FROM series JOIN series ON series.character_id = character_id WHERE chacter ='human' 
+   ORDER BY LENGTH(characters) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
