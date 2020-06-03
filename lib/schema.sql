45d224552d5ebec1a92a5ff22f5b517d9edcc8d9
Irene Scott <irenejinscott@gmail.com>
2020-06-03 19:08:33 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Series (
+        id INTEGER PRIMARY KEY,
+                name TEXT, 
+                age INTEGER
+            );
+CREATE TABLE Series (
+        id INTEGER PRIMARY KEY,
+                name TEXT, 
+                age INTEGER
+            );
+CREATE TABLE Series (
+        id INTEGER PRIMARY KEY,
+                name TEXT, 
+                age INTEGER
+            );
+CREATE TABLE Series (
+        id INTEGER PRIMARY KEY,
+                name TEXT, 
+                age INTEGER
+            );
